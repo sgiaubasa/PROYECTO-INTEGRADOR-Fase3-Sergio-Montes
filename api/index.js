9b91b0c994c4d502bafbf009028bb857439dc715
Montes <sergio.montes@aubasa.com.ar>
2025-10-19 23:43:48 -0300

    fix(api): actualizar runtime a nodejs para Vercel

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@
 import serverless from "serverless-http";
 import app from "../backend/src/app.js"; // <--- esta es la ruta correcta
 
-export const config = { runtime: "nodejs20.x" };
+export const config = { runtime: "nodejs" };
 export default serverless(app);
 
